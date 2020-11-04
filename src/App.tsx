64ecf35998207c245b6d320fdd3e39fcee13941e
eliasall3x <elliasallex@gmail.com>
2020-11-04 19:02:23 -0300

    Feat: forwardRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, {useRef, useCallback, FormEvent} from 'react';
+import Input from './components/Input'
 
 const App = () => {
-  return <div>Hello World!</div>;
+  const nameInputRef = useRef<HTMLInputElement>(null)
+  const acceptTermsRef = useRef({ value: false })
+
+  const handleSubmit = useCallback((event: FormEvent) => {
+    event.preventDefault()
+
+    console.log(nameInputRef.current?.value)
+    console.log(acceptTermsRef.current.value)
+  }, [])
+
+  const handleAcceptTerms = useCallback(() => {
+    acceptTermsRef.current.value = !acceptTermsRef.current.value
+  }, [])
+
+  return <div className="App">
+    <form action="" onSubmit={handleSubmit}>
+      <Input
+        name="name"
+        label="Nome completo"
+        placeholder="Digite seu nome"
+        ref={nameInputRef}
+      />
+
+      <button type="button" onClick={handleAcceptTerms}>
+        Aceitar termos
+      </button>
+      <button type="submit">
+        Enviar
+      </button>
+    </form>
+  </div>;
 }
 
 export default App;
